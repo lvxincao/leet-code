069e161ae6ab0642b90c778d9f471af8d77788c4
xincao.lv <xincao.lv@zhixun.china.com>
2019-11-07 17:59:21 +0800

    重写第二遍

--- src/main/java/com/donkey/a26/Solution.java
+++ src/main/java/com/donkey/a26/Solution.java
@@ -24,15 +24,15 @@ public class Solution {
         int i = 0;
         for (int j = 1; j < nums.length; j++) {
             if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
+                nums[++i] = nums[j];
             }
         }
         return i + 1;
     }
 
     public static void main(String[] args) {
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+
+        int[] nums = {};
         System.out.println(removeDuplicates(nums));
     }
 }
