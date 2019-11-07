a456ac105fbbd0e5f2940fa02e98a65b80606c39
xincao.lv <xincao.lv@zhixun.china.com>
2019-11-07 10:56:49 +0800

    init

---
+++ src/main/java/com/donkey/a26/Solution.java
@@ -0,0 +1,39 @@
+package com.donkey.a26;
+
+/**
+ * @author: lxc
+ * @Date: 2019/11/6 17:58
+ * @Description:
+ */
+public class Solution {
+
+    /**
+     * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+     * <p>
+     * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+     * <p>
+     * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+     *
+     * @param nums
+     * @return
+     */
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(nums));
+    }
+}
+
